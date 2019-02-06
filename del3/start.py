f1dffd50f91a5fb86ee58d27ae6ab4bc27cd8943
Roman <44116985+romanfinkin@users.noreply.github.com>
2019-02-07 00:13:50 +0300

    +

--- del3/start.py
+++ del3/start.py
@@ -1,13 +1,10 @@
-def check():
-    z = int(input("Enter amount of numbers "))
-    i = 0
-    sum = 0
-    while i < z:
-        k = int(input("Enter integer "))
-        if k % 3 == 0:
-            sum += k
-        i += 1
-    print(sum)
-
-
-check()
+z = int(input("Enter amount of numbers "))
+i = 0
+sum = 0
+print('Enter %d number' % z)
+while i < z:
+    k = int(input())
+    if k % 3 == 0:
+        sum += k
+    i += 1
+print(sum)
