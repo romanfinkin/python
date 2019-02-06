a72e4941e7f4c6660680a4427732dc476d6bb67e
Roman <44116985+romanfinkin@users.noreply.github.com>
2019-02-06 20:07:38 +0300

    Delete def. Read about %d.

--- del3/start.py
+++ del3/start.py
@@ -1,13 +1,9 @@
-def check():
-    z = int(input("Enter amount of numbers "))
-    i = 0
-    sum = 0
-    while i < z:
-        k = int(input("Enter integer "))
-        if k % 3 == 0:
-            sum += k
-        i += 1
-    print(sum)
-
-
-check()
+z = int(input("Enter amount of numbers "))
+i = 0
+sum = 0
+k = int(input('Enter %d numbers' % z))
+while i < z:
+    if k % 3 == 0:
+        sum += k
+    i += 1
+print(sum)
